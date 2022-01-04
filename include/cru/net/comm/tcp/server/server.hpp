0774a92521ed15fa7078b1aa8772b71a92cc70b4
Subash Kannoth <subash.kannoth@iese.fraunhofer.de>
2022-01-04 16:53:35 +0100

    Adds server thread aborter + std::string as data type for callback

--- include/cru/net/comm/tcp/server/server.hpp
+++ include/cru/net/comm/tcp/server/server.hpp
@@ -6,7 +6,9 @@
 #include <iostream>
 #include <memory>
 #include <utility>
+#include <atomic>
 
+#include <cru/utils/log/logger.hpp>
 #include <cru/net/comm/tcp/server/api/i_server.hpp>
 #include <cru/net/comm/tcp/server/session.hpp>
 
@@ -19,7 +21,7 @@ class server final : public api::i_server {
  public:
   server(
       int port,
-      std::function<void(std::time_t time, std::vector<char>)> data_callback);
+      std::function<void(std::time_t time, const std::string&)> data_callback);
   ~server() = default;
 
   virtual void start() override;
@@ -28,20 +30,23 @@ class server final : public api::i_server {
 
  private:
   void doAccept() {
-    acceptor_->async_accept(
-        [this](boost::system::error_code ec, ip::tcp::socket socket) {
-          if (!ec) {
-            std::make_shared<session>(std::move(socket), data_callback_)->start();
-          }
+    acceptor_->async_accept([this](boost::system::error_code ec,
+                                   ip::tcp::socket socket) {
+      if (!ec) {
+        logger_.debugs("Accepted new connection");
+        std::make_shared<session>(std::move(socket), data_callback_)->start();
+      }
 
-          doAccept();
-        });
+      doAccept();
+    });
   }
 
  private:
-  std::function<void(std::time_t time, std::vector<char>)> data_callback_;
+  std::function<void(std::time_t time, const std::string& data)> data_callback_;
   boost::asio::io_context io_context_;
   std::unique_ptr<ip::tcp::acceptor> acceptor_;
+  utils::logger logger_;
+  std::atomic_bool is_running;
 };
 
 }  // namespace tcp
--- src/net/comm/tcp/server/server.cc
+++ src/net/comm/tcp/server/server.cc
@@ -5,18 +5,32 @@ namespace net {
 namespace comm {
 namespace tcp {
 
-server::server(int port, std::function<void(std::time_t time, std::vector<char>)> data_callback)
-    : data_callback_(data_callback) {
-
+server::server(
+    int port,
+    std::function<void(std::time_t, const std::string&)> data_callback)
+    : data_callback_(data_callback),
+      logger_(utils::logger("cru_tcp_server", utils::logger::severity::all)),
+      is_running(true) {
   acceptor_ = std::make_unique<ip::tcp::acceptor>(
       io_context_, ip::tcp::endpoint(ip::tcp::v4(), port));
 
+  logger_.info("Started");
   this->doAccept();
 }
 
-void server::start() {}
+void server::start() {
+  while (!is_running.load()) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    io_context_.stop();
+  }
+  std::thread ioc_thread([&]() { io_context_.run(); });
 
-void server::shutDown() {}
+  ioc_thread.join();
+}
+
+void server::shutDown() { 
+      logger_.trace("Shutting down ...");
+      is_running.store(false); }
 
 void server::detach() {}
 
