1f47d1cc05321cacf0f506f04c47da076ae94f4e
Subash Kannoth <subash.kannoth@iese.fraunhofer.de>
2022-01-04 22:19:50 +0100

    Fixes tcp sever tests

--- test/net/tcp/tcp_server_test.cc
+++ test/net/tcp/tcp_server_test.cc
@@ -22,23 +22,25 @@ class ServerTest : public ::testing::Test {
 
   std::unique_ptr<std::thread> server_thread_;
   std::condition_variable cond_var_;
-  bool booted_ = false;
   std::mutex mutex_;
   std::string server_rx_data_;
 
   void SetUp() override {
     server_ = new server(port_, [&](std::time_t time, const std::string& data) {
+      std::lock_guard<std::mutex> guard(mutex_);
+      cond_var_.notify_one();
       server_rx_data_ = data;
     });
 
     client_ = new client(address_, port_);
 
-    server_thread_ = std::make_unique<std::thread>(
-      [&]() { server_->start(); });
+    server_thread_ = std::make_unique<std::thread>([&]() { server_->start(); });
+
+    server_thread_->detach();
   }
 
   void TearDown() override {
-    server_thread_->join();
+    // server_thread_->join();
   }
 };
 
@@ -46,8 +48,11 @@ TEST_F(ServerTest, SendData) {
   std::string test_string("Hello CRU");
   EXPECT_EQ(std::string(), server_rx_data_);
 
-  client_->send("Hello Server");
+  client_->connect();
+  client_->send(test_string);
 
+  std::unique_lock<std::mutex> lock(mutex_);
+  cond_var_.wait(lock);
   EXPECT_EQ(test_string, server_rx_data_),
 
   server_->shutDown();
